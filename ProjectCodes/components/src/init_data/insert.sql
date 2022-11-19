620111150dc2bf59d955c7e80470dc42a44a735c
cuadah9183 <89544491+cuadah9183@users.noreply.github.com>
2022-11-19 09:07:48 +0530

    Update insert.sql
    inserted default values for user t in profile table

--- ProjectCodes/components/src/init_data/insert.sql
+++ ProjectCodes/components/src/init_data/insert.sql
@@ -3,3 +3,6 @@ INSERT INTO recipe (name, calories, protein, fiber, sodium) VALUES ('Casserole',
 INSERT INTO log (recipeID, userID, time, servings) VALUES (1, 1, current_timestamp, 2), (2, 1, current_timestamp + interval '1 hours', 2);
 
 INSERT INTO library (recipeID, userID) VALUES (1, 1), (2, 1);
+
+INSERT INTO profile (username, mydescription, favorites)
+    VALUES ('t','I am a resident of Colorado and fond of hiking and skiing', 'I love lemon cake!');
