0c35faf701a1366aaa7f90d5ab3ecabc994d5d78
Saphire Young <saphire.young@colorado.edu>
2022-11-27 13:13:45 -0700

    updated add to library form, test data

--- ProjectCodes/components/src/init_data/insert.sql
+++ ProjectCodes/components/src/init_data/insert.sql
@@ -1,6 +1,6 @@
-INSERT INTO recipe (name, calories, protein, fat, carbs) VALUES ('Casserole', 450, 18, 12, 280), ('Tea', 120, 4, 0, 50);
+INSERT INTO recipe (name, calories, protein, fat, carbohydrates) VALUES ('Casserole', 450, 18, 12, 280), ('Tea', 120, 4, 0, 50);
 
-INSERT INTO log (recipeID, userID, time, servings) VALUES (1, 1, current_timestamp, 2), (2, 1, current_timestamp + interval '1 hours', 2);
+INSERT INTO log (recipeID, userID, time, servings) VALUES (1, 1, current_timestamp - interval '7 hours', 2), (2, 1, current_timestamp - interval '6 hours', 2), (1, 1, current_timestamp - interval '2 days', 1), (1, 1, current_timestamp - interval '8 days', 1);
 
 INSERT INTO library (recipeID, userID) VALUES (1, 1), (2, 1);
 
